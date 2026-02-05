b87ba45e857dd8f4c5bbc2a6c4cd8b4cc6fedb35
mdtayobali13 <mdtayobalideveloper@gmail.com>
2026-02-05 17:34:36 +0600

    successfully

--- lib/views/auth/sign_in_screen.dart
+++ lib/views/auth/sign_in_screen.dart
@@ -304,7 +304,7 @@ class _SignInScreenState extends State<SignInScreen> {
                         title: "Login Failed",
                         message:
                             viewModel.loginResponse?.message ??
-                            "Please check your email and password ${viewModel.loginResponse?.message}",
+                            "Please check your email and password ${viewModel.loginResponse?.status}",
                       );
                     }
                   }
